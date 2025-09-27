b7ceae977553428a9e55297152235567acb4530f
Bob <insight@gmail.com>
2025-09-27 18:33:03 +0900

    Pipeline

--- scripts/data_load.py
+++ scripts/data_load.py
@@ -1,1 +1,3 @@
-pandas==3.1.2
+version https://git-lfs.github.com/spec/v1
+oid sha256:e6804174313fad9463823c09e40c4f70a0048c9a3e8227a5dc51f5593223f548
+size 13
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,34 +1,3 @@
-import pandas as pd
-import os
-class DataLoader:
-    """
-    Class to load and merge CSV datasets from multiple league folders inside Data/Raw_Data.
-    """
-
-    def __init__(self, raw_data_path="Data/Raw_Data/"):
-        self.raw_data_path = raw_data_path
-
-    def load_league_files(self):
-        """
-        Loads all CSV files from each league folder and returns a list of DataFrames.
-        """
-        df_list = []
-        # Loop over each league folder
-        for league_folder in os.listdir(self.raw_data_path):
-            league_path = os.path.join(self.raw_data_path, league_folder)
-            if os.path.isdir(league_path):  # ensure it's a folder
-                # Loop over CSV files in this folder
-                for file in os.listdir(league_path):
-                    if file.endswith(".csv"):
-                        file_path = os.path.join(league_path, file)
-                        df = pd.read_csv(file_path)
-                        df_list.append(df)
-        return df_list
-
-    def merge_all_leagues(self):
-        """
-        Merge all league datasets into a single DataFrame.
-        """
-        df_list = self.load_league_files()
-        merged_df = pd.concat(df_list, ignore_index=True)
-        return merged_df
+version https://git-lfs.github.com/spec/v1
+oid sha256:d6c828a09b2f6c0059819897a52d03ea7d745b38d671aa246928293278dcb63d
+size 1275
