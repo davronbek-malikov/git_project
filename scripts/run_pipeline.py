13b39cc0154597dbdaab2dab67219ef7484a3c5d
Bob <insight@gmail.com>
2025-09-29 14:42:44 +0900

    Dataset yuklandi

---
+++ scripts/run_pipeline.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4a10b198cb056147ff762cfeb83bd8501c940d0babb6f3b23f704e58f9302621
+size 433
---
+++ src/pipeline.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c07937c6b842a5017866639f4e0929e79343b9b2a1adbfef81bc926b37744b68
+size 1108
